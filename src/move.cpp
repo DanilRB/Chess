1c53e2374ba55339a77de4bc7b256f8cd951a059
DanilRB <danil.rubnikov@mail.ru>
2019-04-30 17:55:11 +0700

    create move figure

--- src/move.cpp
+++ src/move.cpp
@@ -1,34 +1,18 @@
 #include "foo.h"
 #include <iostream>
 
-using namespace std;
-
-void convert(char s[])
+void move(char* dos[], char s[])
 {
-    for (int i = 0; i < 5; i += 3) {
-        s[i] -= 49;
-    }
-}
-
-void move(char* dos[])
-{
-    char s[6];
-L:
-    cin >> s;
-    if (s[5] != '\0')
-        cout << "Error input" << endl;
-    convert(s);
-
-    for (int i = 0; i < 5; i++) {
-        if (!(s[i] >= 49 && s[i] <= 56)) {
-            cout << "Error input" << endl;
-            goto L;
-        }
-        if (i == 1)
-            i++;
-    }
-
-    if (s[0] == s[3] && (s[4] - s[1] == 1 || (s[0] == 1 && s[4] - s[1] == 2)))
-        swap(dos[(int)(char)s[1]][(int)(char)s[0]],
-             dos[(int)(char)s[4]][(int)(char)s[3]]);
+    if (s[0] == s[3] && (s[4] - s[1] == 1 || (s[0] == 1 && s[4] - s[1] == 2))
+        && dos[(int)(char)s[4]][(int)(char)s[3]] == '0')
+        std::swap(
+                dos[(int)(char)s[1]][(int)(char)s[0]],
+                dos[(int)(char)s[4]][(int)(char)s[3]]);
+    else if (
+            s[0] == s[3]
+            && (s[1] - s[4] == 1 || (s[0] == 6 && s[1] - s[4] == 2))
+            && dos[(int)(char)s[4]][(int)(char)s[3]] == '0')
+        std::swap(
+                dos[(int)(char)s[1]][(int)(char)s[0]],
+                dos[(int)(char)s[4]][(int)(char)s[3]]);
 }
